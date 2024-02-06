440412623062bbfce15b7959f10849fed8d1b79e
Trewor-Vincent <Trewor.Ploomipuu@tktk.ee>
2024-02-06 16:55:47 +0200

    Week2

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+Console.WriteLine("Enter your age:");
+
+string userAge = Console.ReadLine();
+
+int userAgeNum = 0;
+
+bool isAgeNumber = Int32.TryParse(userAge, out userAgeNum);
+
+//int userAge = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine($"Parse result {userAgeNum}. User is {userAgeNum} years old.");
+
+if(isAgeNumber)
+{
+    if(userAgeNum >= 13)
+    {
+        Console.WriteLine("Welcome to instagram.");
+    }
+    else
+    {
+        Console.WriteLine("You are too young to use Instagram.");
+    }
+}
+else
+{
+    Console.WriteLine("Could not read your age.");
+}
